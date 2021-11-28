5a5005c6ebfff441aad0da3f08e7255e9b4e11cf
SACHIN B S <94137357+SachinGowda1208@users.noreply.github.com>
2021-11-29 00:32:20 +0530

    To find Area
    Program to find the area of cross section of Rectangle, Circle and Triangle by Using Python programs.

---
+++ Area Recta Circle Triangle.py
@@ -0,0 +1,28 @@
+#wap to find area of Rectangle:-
+"""
+class rectangle():
+    length = 80
+    breadth = 50
+
+    def area(self):
+        print(f'area of rectangle is {self.length*self.breadth}')
+
+#wap to find area of Circle:-
+
+class Circle():
+    radius = 5
+    pie = 3.14
+
+    def area(self):
+        print(f'area of circle with {self.radius} is {self.pie * self.radius**2}')
+
+#wap to find area of triangle:-
+
+class triangle():
+    base = 10
+    hight = 6
+    x = 1/2
+
+    def area(self):
+        print(f'area of the triangle is {self.x*self.base*self.hight}')
+"""
